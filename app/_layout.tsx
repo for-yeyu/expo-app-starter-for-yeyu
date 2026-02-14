e079e9e1296f2b8039d05d0fca1ebd110b4dd9d5
Ye Yu <nearjilt@gmail.com>
2026-02-14 11:25:17 +0800

    feat: 😋 add basic example pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="dark" backgroundColor="#ffffff" />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'A',
+          }}
+        />
+        <Tabs.Screen
+          name="b"
+          options={{
+            title: 'B',
+          }}
+        />
+      </Tabs>
+    </>
+  );
 }
